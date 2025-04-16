e70215d171bc6df0f17dfefd892a97fdfaa196fa
NashatKNDSU <nashat.khan@ndsu.edu>
2025-04-15 21:29:32 -0500

    #added for loop to go thru the USERS array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactTitleComponent } from "./contact-title/contact-title.component";
+import { user } from './model/user';
+import { USERS } from './data/user-data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactTitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'assignment-4';
+  contacts = USERS;
+
 }
--- src/app/contact-title/contact-title.component.ts
+++ src/app/contact-title/contact-title.component.ts
@@ -1,12 +1,27 @@
 import { Component, Input } from '@angular/core';
 import { user } from '../model/user';
+import { USERS } from '../data/user-data';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-contact-title',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-title.component.html',
   styleUrl: './contact-title.component.css'
 })
 export class ContactTitleComponent {
   @Input() user!: user;
+  
+
+  // deleteUser()
+  // {
+   
+  // }
+  // editUser()
+  // {
+   
+  // }
 
 }
+
+
