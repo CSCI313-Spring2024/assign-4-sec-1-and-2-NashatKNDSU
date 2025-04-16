a428a76968f9b7c414a7b43b2d3bf8fd97abbbc9
NashatKNDSU <nashat.khan@ndsu.edu>
2025-04-16 15:40:57 -0500

    #final edits and formatting!

--- src/app/contact-title/contact-title.component.ts
+++ src/app/contact-title/contact-title.component.ts
@@ -3,10 +3,11 @@ import { user } from '../model/user';
 import { USERS } from '../data/user-data';
 import { FormsModule } from '@angular/forms';
 import { ContactServiceService } from '../contact-service.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact-title',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './contact-title.component.html',
   styleUrl: './contact-title.component.css'
 })
--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ContactServiceService } from '../contact-service.service';
+import { USERS } from '../data/user-data';
 
 @Component({
   selector: 'app-edit-contact',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './edit-contact.component.html',
   styleUrl: './edit-contact.component.css'
 })
-export class EditContactComponent {
+export class EditContactComponent implements OnInit {
 
+
+  id!: number;
+
+  firstname: string = '';
+  lastname: string = '';
+  phonenumber: string = '';
+  email: string = '';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.id = +this.route.snapshot.paramMap.get('id')!;
+    this.firstname = USERS[this.id].firstname;
+    this.lastname = USERS[this.id].lastname;
+    this.phonenumber = USERS[this.id].phonenumber;
+    this.email = USERS[this.id].email;
+  }
+
+  contactsService = inject(ContactServiceService);
+
+  UpdatedContact(){
+    this.contactsService.updateContact(this.id, this.firstname, this.lastname, this.phonenumber, this.email);
+  }
 }
